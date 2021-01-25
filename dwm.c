36d8a28d4dc6f0630677d396055f38c9ad99ba09
cameron <cameron.murtagh00@gmail.com>
2021-01-26 10:00:37 +1100

    added status on all monitors

--- dwm.c
+++ dwm.c
@@ -799,11 +799,11 @@ drawbar(Monitor *m)
 		stw = getsystraywidth();
 
 	/* draw status first so it can be overdrawn by tags later */
-	if (m == selmon) { /* status is only drawn on selected monitor */
-		drw_setscheme(drw, scheme[SchemeNorm]);
-		tw = TEXTW(stext) - lrpad / 2 + 2; /* 2px right padding */
-		drw_text(drw, m->ww - tw - stw, 0, tw, bh, lrpad / 2 - 2, stext, 0);
-	}
+	/* status is only drawn on selected monitor */
+	drw_setscheme(drw, scheme[SchemeNorm]);
+	tw = TEXTW(stext) - lrpad / 2 + 2; /* 2px right padding */
+	drw_text(drw, m->ww - tw - stw, 0, tw, bh, lrpad / 2 - 2, stext, 0);
+
 
 	resizebarwin(m);
 	for (c = m->clients; c; c = c->next) {
@@ -2202,7 +2202,7 @@ updatestatus(void)
 {
 	if (!gettextprop(root, XA_WM_NAME, stext, sizeof(stext)))
 		strcpy(stext, "dwm-"VERSION);
-	drawbar(selmon);
+	drawbars();
 	updatesystray();
 }
 
